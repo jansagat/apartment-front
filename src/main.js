2be8d684af010ca5ac66b0e8bcb469daebee6db9
JanSagat <erzhan.sagyt@gmail.com>
2018-12-04 21:33:05 +0600

    Added firebase

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import Vue from 'vue'
+import '@/plugins/vuetify'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import firebaseConfig from '@/config/firebase.config'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+firebase.initializeApp(firebaseConfig)
+firebase.auth().languageCode = 'en'
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')

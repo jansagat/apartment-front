b463057ab8d832a17d6767999458a177b3d55a37
Yerzhan Sagyt <erzhan.sagyt@gmail.com>
2018-12-14 17:49:12 +0600

    Added app logo component

---
+++ src/components/_common/AppLogo.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'AppLogo'
+}
+</script>
+
+<template>
+  <v-img
+    :src="require('@/assets/logo.png')"
+    contain
+    max-height="70"
+  ></v-img>
+</template>
+
+<style scoped>
+
+</style>

fa7ff86956bd1d179d27d10a0a01a35028868b4e
Yerzhan Sagyt <erzhan.sagyt@gmail.com>
2018-12-14 17:55:29 +0600

    Added SendSms & SignIn components

---
+++ src/views/Auth/SendSms.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'SendSms',
+  data () {
+    return {
+      phoneNumber: null,
+      smsSent: false,
+      phoneDisabled: false
+    }
+  },
+  methods: {
+    sendSMS () {
+      this.$router.push({ name: 'SignIn' })
+    }
+  }
+}
+</script>
+
+<template>
+ <div>
+   <app-logo/>
+   <v-text-field
+     label="Номер телефона"
+     v-model="phoneNumber"
+     type="tel"
+     :disabled="phoneDisabled"
+   ></v-text-field>
+   <v-btn
+     block
+     depressed
+     color="yellow"
+     @click="sendSMS"
+     v-show="!smsSent"
+   >
+     NEXT
+   </v-btn>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Auth/SignIn.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'SignIn',
+  data () {
+    return {
+      confirmCode: null
+    }
+  },
+  methods: {
+    signIn () {
+      this.$emit('signIn')
+    },
+    back () {
+      this.$router.push({ name: 'SendSms' })
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <app-logo/>
+    <v-text-field
+      label="Код подтверждения"
+      v-model="confirmCode"
+      type="tel"
+      :messages="'sms sent text'"
+    ></v-text-field>
+    <v-btn
+      block
+      depressed
+      color="yellow"
+      @click="signIn"
+    >
+      NEXT
+    </v-btn>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

6054e409b2542196994443b5a0c52e61f5085668
JanSagat <erzhan.sagyt@gmail.com>
2019-01-06 23:13:40 +0600

    Added newAnnouncement store

---
+++ src/store/modules/newAnnouncement.js
@@ -0,0 +1,39 @@
+const state = {
+  newAnnouncement: null,
+  newAnnouncementImages: null
+}
+
+const getters = {
+  getNewAnnouncement (state) {
+    return state.newAnnouncement
+  },
+  getNewAnnouncementImages (state) {
+    return state.newAnnouncementImages
+  }
+}
+
+const mutations = {
+  setNewAnnouncement (state, newAnnouncement) {
+    state.newAnnouncement = newAnnouncement
+  },
+  setNewAnnouncementImages (state, newAnnouncementImages) {
+    state.newAnnouncementImages = newAnnouncementImages
+  }
+}
+
+const actions = {
+  setNewAnnouncement ({ commit }, newAnnouncement) {
+    commit('setNewAnnouncement', newAnnouncement)
+  },
+  setNewAnnouncementImages ({ commit }, newAnnouncementImages) {
+    commit('setNewAnnouncementImages', newAnnouncementImages)
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,22 +1,32 @@
 const state = {
-  isUserSignedIn: false
+  isUserSignedIn: false,
+  userInfo: null
 }
 
 const getters = {
   getAuthState (state) {
     return state.isUserSignedIn
+  },
+  getUserInfo (state) {
+    return state.userInfo
   }
 }
 
 const mutations = {
   setAuthState (state, authState) {
     state.isUserSignedIn = authState
+  },
+  setUserInfo (state, userInfo) {
+    state.userInfo = userInfo
   }
 }
 
 const actions = {
   setAuthState ({ commit }, authState) {
     commit('setAuthState', authState)
+  },
+  setUserInfo ({ commit }, userInfo) {
+    commit('setUserInfo', userInfo)
   }
 }
 

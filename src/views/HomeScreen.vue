c415c0724388dcff170daf51a82522b8cf610889
Yerzhan Sagyt <erzhan.sagyt@gmail.com>
2019-01-11 17:46:07 +0600

    Setting user info

--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -1,17 +1,27 @@
 <script>
 import firebase from 'firebase/app'
 import store from '@/store'
+import { mapGetters } from 'vuex'
 const USER_SIGNED_IN = true
 const NO_USER_SIGNED_IN = false
 
 export default {
   name: 'HomeScreen',
+  computed: {
+    ...mapGetters('user', ['getUserInfo'])
+  },
   async beforeRouteEnter (to, from, next) {
     try {
-      await firebase.auth().onAuthStateChanged((user) => {
-        user
-          ? store.dispatch('user/setAuthState', USER_SIGNED_IN)
-          : store.dispatch('user/setAuthState', NO_USER_SIGNED_IN)
+      await new Promise((resolve) => {
+        firebase.auth().onAuthStateChanged((user) => {
+          user
+            ? store.dispatch('user/setAuthState', USER_SIGNED_IN) &&
+            store.dispatch('user/setUserInfo', {
+              phone: user.phoneNumber
+            })
+            : store.dispatch('user/setAuthState', NO_USER_SIGNED_IN)
+          resolve()
+        })
       })
     } catch (e) {
       console.error(e)

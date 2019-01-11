0ce590b26fcc174fb32817ad62b740b6bbafb308
Yerzhan Sagyt <erzhan.sagyt@gmail.com>
2019-01-11 08:31:43 +0600

    Added infinity scroll

--- src/views/Announcements/AnnouncementsList.vue
+++ src/views/Announcements/AnnouncementsList.vue
@@ -1,6 +1,8 @@
 <script>
 import AnnouncementCard from '@/components/Announcements/AnnouncementCard'
 import AnnouncementsFilterButton from '@/components/Announcements/AnnouncementsFilterButton'
+import firebase from 'firebase/app'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'AnnouncementsList',
@@ -13,23 +15,79 @@ export default {
   },
   data () {
     return {
-      count: 5
+      screenBottom: false,
+      lastDoc: null
     }
   },
+  computed: {
+    ...mapGetters('announcements', ['getAnnouncements'])
+  },
+  watch: {
+    screenBottom (val) {
+      if (val) {
+        this.fetchNextAnnouncements()
+      }
+    }
+  },
+  mounted () {
+    document.addEventListener('scroll', this.isScreenBottomDisplayed)
+    this.fetchAnnouncementsFirstPage()
+  },
+  destroyed () {
+    document.removeEventListener('scroll', this.isScreenBottomDisplayed)
+  },
   methods: {
+    isScreenBottomDisplayed () {
+      let rect = document.querySelector('.end').getBoundingClientRect()
+      let elemTop = rect.top
+      let elemBottom = rect.bottom
+
+      this.screenBottom = elemTop < window.innerHeight && elemBottom >= 0
+    },
+    async fetchAnnouncementsFirstPage () {
+      try {
+        let announcements = []
+        const db = firebase.firestore()
+        const querySnapshots = await db.collection('announcements').limit(10).get()
+        this.lastDoc = querySnapshots.docs[querySnapshots.docs.length - 1]
+        querySnapshots.forEach((doc) => {
+          announcements.push(doc.data())
+        })
+        this.setAnnouncements(announcements)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    async fetchNextAnnouncements () {
+      try {
+        let announcements = []
+        const db = firebase.firestore()
+        if (!this.lastDoc) return
+        const querySnapshots = await db.collection('announcements')
+          .startAfter(this.lastDoc)
+          .limit(10)
+          .get()
+        this.lastDoc = querySnapshots.docs[querySnapshots.docs.length - 1]
+        querySnapshots.forEach((doc) => {
+          announcements.push(doc.data())
+        })
+        this.concatAnnouncements(announcements)
+      } catch (e) {
+        console.error(e)
+      }
+    },
     onClickOpenFilterAnnouncements () {
       this.$router.push({ name: 'FilterAnnouncements' })
-    }
+    },
+    ...mapActions('announcements', ['setAnnouncements', 'concatAnnouncements'])
   }
 }
 </script>
 
 <template>
   <div class="announcements-list">
-    <h1>AnnouncementsList</h1>
-
     <announcement-card
-      v-for="(announcement, index) in count"
+      v-for="(announcement, index) in getAnnouncements"
       :announcement="announcement"
       :id="index"
       :key="index"
@@ -38,6 +96,8 @@ export default {
     <announcements-filter-button
       @click.native="onClickOpenFilterAnnouncements"
     />
+
+    <div class="end"></div>
   </div>
 </template>
 

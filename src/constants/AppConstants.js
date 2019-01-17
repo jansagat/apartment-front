e65915b64e5e47fce7c5c06008d31d49f4f93e2a
Yerzhan Sagyt <erzhan.sagyt@gmail.com>
2019-01-17 15:11:34 +0600

    Added other constants in AppConstants

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -1,7 +1,24 @@
-class AppConstants {
+const SEC_IN_MS = 1000
+const MIN_IN_SEC = 60
+
+const CITIES = [
+  { id: 1, name: 'Астана' },
+  { id: 2, name: 'Алматы' }
+]
+const PROPERTY_TYPE = [
+  { id: 1, name: 'квартиру' },
+  { id: 2, name: 'дом' },
+  { id: 3, name: 'другой тип недвиж.' }
+]
+const OFFER_TYPE = [
+  { id: 1, name: 'Сдаю' },
+  { id: 2, name: 'Продаю' }
+]
+
+export {
+  SEC_IN_MS,
+  MIN_IN_SEC,
+  CITIES,
+  PROPERTY_TYPE,
+  OFFER_TYPE
 }
-
-AppConstants.SEC_IN_MS = 1000
-AppConstants.MIN_IN_SEC = 60
-
-export default AppConstants

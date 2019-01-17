f107c92c54389b34bd0e8480dcb3ab44f8b4f51e
Yerzhan Sagyt <erzhan.sagyt@gmail.com>
2019-01-17 14:57:10 +0600

    Added swiper js

---
+++ src/views/Announcements/AnnouncementSingleImages.vue
@@ -0,0 +1,59 @@
+<script>
+import Swiper from 'swiper'
+export default {
+  name: 'AnnouncementSingleImage',
+  props: {
+    images: Array,
+    initImageIndex: Number
+  },
+  data () {
+    return {
+      swiperInstance: null
+    }
+  },
+  mounted () {
+    this.swiperInstance = new Swiper(this.$refs.swiper, {
+      zoom: true
+    })
+  }
+}
+</script>
+
+<template>
+  <div class="images-container">
+      <div ref="swiper" class="swiper-container">
+        <div class="swiper-wrapper">
+          <div
+            v-for="(image, index) in images"
+            :key="index"
+            class="swiper-slide"
+          >
+            <div class="swiper-zoom-container">
+              <img :src="image">
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+@import '@/styles/swiper.sass'
+
+.swiper-container
+  width: 100%
+  height: 100%
+
+.swiper-slide
+  overflow: hidden
+  
+.images-container
+  overflow: hidden
+  position: absolute
+  top: 0
+  left: 0
+  bottom: 0
+  right: 0
+  background: #000
+  
+</style>
